b20479491b8fb7aa8b6d25dc2afe1185b9092cfe
Marcin <marcin.szuwalski@motorola.com>
2022-10-03 12:10:33 +0200

    IntelliJ start

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n" +
+                "┌─┐┌─┐┬─┐  ┬─┐┌─┐┌─┐┌─┐\n" +
+                "│  ├─┤├┬┘  ├┬┘├─┤│  ├┤ \n" +
+                "└─┘┴ ┴┴└─  ┴└─┴ ┴└─┘└─┘");
+        System.out.println("Gentlemen start your engines");
+    }
+
+/*
+    Where does Gentlemen start your engines come from?
+    The first time that the term, “Gentlemen, start your engines”
+    appeared in print on the race morning “countdown” schedule was in 1953,
+    and while the claim in Wilbur Shaw's memoirs is that he had said it every year since 1946,
+    it appears that he did not do so until 1953.
+*/
+
+}
